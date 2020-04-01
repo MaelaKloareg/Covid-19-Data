b538edb16318ecca7ddd141793a5fc1048ef7eb9
Maela Kloareg <42865360+MaelaKloareg@users.noreply.github.com>
2020-04-02 00:33:56 +0200

    Add files via upload

--- script.covid.world.evolution.r
+++ script.covid.world.evolution.r
@@ -8,6 +8,10 @@ library(readxl)
 library(httr)
 library(ggplot2)
 
+aujourdhui=format(Sys.Date(),"%d/%m%/%Y")
+hier=format(Sys.Date()-1,"%d/%m%/%Y")
+dateconfinement = as.POSIXct(as.Date("2020-03-17"))
+
 # create the URL where the dataset is stored with automatic updates every day
 
 url <- paste("https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-",format(Sys.time(), "%Y-%m-%d"), ".xlsx", sep = "")
@@ -52,12 +56,11 @@ sort(tapply(data$cumulDeaths, data$Country, max, na.rm=TRUE), decreasing = TRUE)
 # my selection of countries
 toto=droplevels(subset(data, 
 	Country%in%c("France","Germany","Italy","Spain","United_Kingdom", 
-	"Netherlands", "Switzerland", "Belgium", "United_States_of_America",
-	"Iran")
+	"Netherlands", "Switzerland", "Belgium", "United_States_of_America")
 	&dateRep>"2020-02-24 UTC"))
 
 # optional french translation
-levels(toto$Country)=c("Belgique","France", "Allemagne", "Iran","Italie", "Pays-Bas", "Espagne", "Suisse", "Royaume-Uni", "Etats-Unis")
+levels(toto$Country)=c("Belgique","France", "Allemagne","Italie", "Pays-Bas", "Espagne", "Suisse", "Royaume-Uni", "Etats-Unis")
 summary(toto)
 
 
@@ -72,41 +75,49 @@ toto$Country=factor(toto$Country,CountryNewLevels)
 ggplot(data=toto) + aes(y=cumulDeaths, x=dateRep, col=Country) +geom_point() + geom_line() + theme_classic() +
 	scale_y_continuous(trans = 'log10') + 
 	annotation_logticks(sides="l") + 
-	labs(y="Nombre total de morts (échelle logarithmique)", x="Date", col="Pays")
+	labs(y="Nombre total de morts (échelle logarithmique)", x="Date", col="Pays")+ 
+  scale_colour_brewer(palette="Set1")
 
-# linear adjustment on log scale, for total number of deaths > 10
-tutu = subset(toto, cumulDeaths>10)
 
-ggplot(data=tutu) + aes(y=cumulDeaths, x=dateRep, group=Country, col=Country) +theme_classic() +
+# same for total number of deaths > 10
+
+ggplot(data=subset(toto, cumulDeaths>10)) + aes(y=cumulDeaths, x=dateRep, group=Country, col=Country) +theme_classic() +
   geom_point() + 
 #  geom_smooth(method='lm', se=FALSE, colour="grey") + 
   geom_line() + 
   scale_y_continuous(trans = 'log10') + 
   annotation_logticks(sides="l") + 
-  labs(y="Nombre total de morts (échelle logarithmique)", x="Date", col="Pays")
+  labs(y="Nombre total de morts (échelle logarithmique)", x="Date", col="Pays")+ 
+  scale_colour_brewer(palette="Set1")
+
 
 # without log scale
 ggplot(data=tutu) + aes(y=cumulDeaths, x=dateRep, col=Country) +geom_point() + geom_line() + theme_classic() +
-	labs(y="Nombre total de morts", x="Date", col="Pays")
+	labs(y="Nombre total de morts", x="Date", col="Pays")+ 
+  scale_colour_brewer(palette="Set1")
+
 ###
 # Cases
 
-ggplot(data=toto, aes(y=cumulCases, x=dateRep, col=Country)) + theme_classic() +
+ggplot(data=subset(toto, cumulCases>100), aes(y=cumulCases, x=dateRep, col=Country)) + theme_classic() +
   geom_point() + geom_line() + 
   scale_y_continuous(trans = 'log10') +
   annotation_logticks(sides="l") + 
-  labs(y="Nombre total de cas déclarés (échelle logarithmique)", x="Date", col="Pays")
+  labs(y="Nombre total de cas déclarés (échelle logarithmique)", x="Date", col="Pays")+ 
+  scale_colour_brewer(palette="Set1")
+
 
 ###
 # Deaths per million habitant (popData2018)
 
 toto$cumulDeaths_per_million = toto$cumulDeaths/toto$popData2018*1000000
 
-tata = subset(toto, cumulDeaths_per_million>1)
-ggplot(data=tata) + aes(y=cumulDeaths_per_million, x=dateRep, col=Country) +geom_point() + geom_line() + theme_classic() +
+ggplot(data=subset(toto, cumulDeaths_per_million>1)) + aes(y=cumulDeaths_per_million, x=dateRep, col=Country) +geom_point() + geom_line() + theme_classic() +
 	scale_y_continuous(trans = 'log10') + 
 	annotation_logticks(sides="l") + 
-	labs(y="Nombre total de morts par million d'habitant (échelle logarithmique)", x="Date", col="Pays")
+	labs(y="Nombre total de morts par million d'habitant (échelle logarithmique)", x="Date", col="Pays")+ 
+  scale_colour_brewer(palette="Set1")
+
 
 # nb : compared to the previous plot, cf
 # https://twitter.com/JonMinton/status/1239832173450010624 : 
@@ -115,5 +126,20 @@ ggplot(data=tata) + aes(y=cumulDeaths_per_million, x=dateRep, col=Country) +geom
 #log(rate) = log(n/N) = log(n) - log(N)
 #So the denom only shifts the intercept, when at the moment what's of interest is the gradient and trend.
 
+
+#############################
+# daily increase in France
+
+ggplot(data=subset(toto, dateRep>"2020-03-08 01:00:00 CET" & Country=="France")) + theme_classic() +
+	aes(y=cases, x=dateRep) + geom_col(fill="royalblue") +
+	ggtitle("Augmentation quotidienne du nombre de cas COVID-19 en France") +
+	labs(y="", caption=paste("données mises à jour le",hier))+ 
+	annotate("text", x = dateconfinement , y = 2500, label = "Début du confinement", angle = 90)
+
+ggplot(data=subset(toto, dateRep>"2020-03-08 01:00:00 CET" & Country=="France")) + theme_classic() +
+	aes(y=deaths, x=dateRep) + geom_col(fill="darkorchid") +
+	ggtitle("Augmentation quotidienne du nombre de décès dus au COVID-19 en France") +
+	labs(y="", caption=paste("données mises à jour le",hier))+ 
+	annotate("text", x = dateconfinement , y = 120, label = "Début du confinement", angle = 90)
 
 
